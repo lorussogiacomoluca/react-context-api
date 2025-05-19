a96a705b1d666e6e6651dd5b01f01f100ae99d36
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-19 19:21:42 +0200

    [UPD] Wrapping App.jsx with AlertProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import PostsPage from "./components/PostsPage";
 import PostsContext from "./contexts/PostsContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayouts from "./Layouts/DefaultLayouts";
+import { AlertProvider } from "./contexts/AlertContext";
 
 function App() {
   console.log(posts);
 
   return (
-    <PostsContext.Provider value={{ posts }}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayouts />}>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/posts" element={<PostsPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </PostsContext.Provider>
+    <AlertProvider>
+      <PostsContext.Provider value={{ posts }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayouts />}>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/posts" element={<PostsPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PostsContext.Provider>
+    </AlertProvider>
   );
 }
 
