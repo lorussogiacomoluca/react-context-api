d65ca024efb27f1d3befe28aaf814998b142a799
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-19 18:21:40 +0200

    [UPD] Wrap App with CountContext.Provider and pass posts as value

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { posts } from "./assets/posts";
 //Import Context
 import PostsContext from "./contexts/PostsContext";
+import { BrowserRouter, Routes, route } from "react-router-dom";
 
 function App() {
   console.log(posts);
   return <h1>Homepage</h1>;
+
+  <CountContext.Provider value={{ posts }}>
+    <BrowserRouter></BrowserRouter>
+  </CountContext.Provider>;
 }
 
 export default App;
