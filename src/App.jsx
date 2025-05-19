e4455fccebd06ce7da694771f6503c0ffd423b59
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-19 18:29:14 +0200

    [UPD] Create Route System

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { posts } from "./assets/posts";
+import Homepage from "./components/Homepage";
+import PostPage from "./components/PostPage";
 //Import Context
 import PostsContext from "./contexts/PostsContext";
-import { BrowserRouter, Routes, route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   console.log(posts);
-  return <h1>Homepage</h1>;
 
-  <CountContext.Provider value={{ posts }}>
-    <BrowserRouter></BrowserRouter>
-  </CountContext.Provider>;
+  return (
+    <PostsContext.Provider value={{ posts }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/posts" element={<PostPage />} />
+        </Routes>
+      </BrowserRouter>
+    </PostsContext.Provider>
+  );
 }
 
 export default App;
