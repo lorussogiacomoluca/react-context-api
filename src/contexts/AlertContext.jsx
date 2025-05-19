3c7d6cca55aaa9f5b66581ff4a47135eb06ace3e
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-19 19:20:36 +0200

    [BONUS] Define AlertContext.js

---
+++ src/contexts/AlertContext.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+
+const AlertContext = useContext();
+
+//CustomProvider
+const AlertProvider = ({ children }) => {
+  //Definire lo stato iniziale del provider, in cui passare il tipo di alert da generare
+  const initialData = { type: "", message: "" };
+
+  //Definizione della variabile di stato
+  const [alertData, setAlertData] = useState(initialData);
+
+  //restituzione del provider
+  return (
+    <AlertContext.Provider value={{ alertData, setAlertData }}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+//Custom Hook
+const useAlertContext = () => {
+  const context = useContext(AlertContext);
+  return context;
+};
+
+//Export
+export { AlertProvider, useAlertContext };
